3c9f1ca40198bd08b4c08be8cb76bcab6f39d2ee
denismend <denisladeira1@gmail.com>
2022-03-15 21:34:34 -0300

    refactor: :recycle: github layout page

--- app/features/github/Github.api.ts
+++ app/features/github/Github.api.ts
@@ -1,18 +1,22 @@
 import invariant from "tiny-invariant";
 import pick from "lodash/pick";
 import { Types } from ".";
+import { secrets } from "../../../secrets";
 
 const config = {
   headers: {
     accept: "application/vnd.github.v3+json",
-    Authorization: `token ${process.env.GITHUB_API_TOKEN}`,
+    Authorization: `token ${secrets.GITHUB_API_TOKEN}`,
   },
 };
 
-export const getGithubUser = async (username?: string) => {
+export const getUser = async (username?: string) => {
   invariant(username, "Please provide an username as a string");
 
-  const res = await fetch(`https://api.github.com/users/${username}`, config);
+  const res = await fetch(
+    `https://api.github.com/users/${username.toLocaleLowerCase()}`,
+    config
+  );
 
   return pick(await res.json(), ["login", "avatar_url", "html_url", "bio"]);
 };
@@ -52,6 +56,6 @@ export const getCommits = async (
   return (await res.json()).map((commit: Types.Commits.ApiResponse) => ({
     sha: commit.sha,
     message: commit.commit.message,
-    hmtl_url: commit.html_url,
+    html_url: commit.html_url,
   }));
 };
